136d73a2d79828a5382d93fe6dfb49cdb409dede
cluelessoptimus <jayrawat509@gmail.com>
2026-01-19 12:13:41 +0530

    Initial backend setup for EazyFind (Go)

---
+++ internal/search/model.go
@@ -0,0 +1,8 @@
+package search
+
+type Place struct {
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
